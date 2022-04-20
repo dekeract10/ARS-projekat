27c062acb12d61df9fb0aeab42c435c6cb7157e7
PetarK37 <komordzic.sr37.2020@uns.ac.rs>
2022-04-20 22:18:08 +0200

    implemented all getHandlers

--- server.go
+++ server.go
@@ -36,12 +36,23 @@ func (ts *Service) createConfigHandler(w http.ResponseWriter, req *http.Request)
 	w.Write([]byte(id))
 }
 
-func (ts *Service) getAllHandler(w http.ResponseWriter, req *http.Request) {
+func (ts *Service) getAllConfigsHandler(w http.ResponseWriter, req *http.Request) {
 	allTasks := [][]*Config{}
 	for _, v := range ts.data {
-		allTasks = append(allTasks, v)
+		if len(v) == 1 {
+			allTasks = append(allTasks, v)
+		}
 	}
+	renderJSON(w, allTasks)
+}
 
+func (ts *Service) getAllGroupssHandler(w http.ResponseWriter, req *http.Request) {
+	allTasks := [][]*Config{}
+	for _, v := range ts.data {
+		if len(v) > 1 {
+			allTasks = append(allTasks, v)
+		}
+	}
 	renderJSON(w, allTasks)
 }
 
@@ -49,6 +60,17 @@ func (ts *Service) getConfigHandler(w http.ResponseWriter, req *http.Request) {
 	id := mux.Vars(req)["id"]
 	task, ok := ts.data[id]
 	if !ok || len(task) > 1 {
+		err := errors.New("key not found")
+		http.Error(w, err.Error(), http.StatusNotFound)
+		return
+	}
+	renderJSON(w, task)
+}
+
+func (ts *Service) getGroupHandler(w http.ResponseWriter, req *http.Request) {
+	id := mux.Vars(req)["id"]
+	task, ok := ts.data[id]
+	if !ok || len(task) == 1 {
 		err := errors.New("key not found")
 		http.Error(w, err.Error(), http.StatusNotFound)
 		return
