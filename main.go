fa67df2a2a49d827fdbba9b5c9169cfca2ae3cb3
Filip Schenk <dekract@protonmail.com>
2022-05-16 23:11:27 +0200

    add versioning

--- main.go
+++ main.go
@@ -19,18 +19,18 @@ func main() {
 	router.StrictSlash(true)
 
 	server := Service{
-		data: map[string][]*Config{},
+		versions: make(map[string]map[string][]*Config),
 	}
 
-	router.HandleFunc("/config/", server.createConfigHandler).Methods("POST")
-	router.HandleFunc("/group/", server.createGroupHandler).Methods("POST")
-	router.HandleFunc("/configs/", server.getAllConfigsHandler).Methods("GET")
-	router.HandleFunc("/groups/", server.getAllGroupsHandler).Methods("GET")
-	router.HandleFunc("/group/{id}", server.getGroupHandler).Methods("GET")
-	router.HandleFunc("/config/{id}", server.getConfigHandler).Methods("GET")
-	router.HandleFunc("/config/{id}/", server.delConfigHandler).Methods("DELETE")
-	router.HandleFunc("/group/{id}/", server.delGroupHandler).Methods("DELETE")
-	router.HandleFunc("/group/{id}/configs", server.putConfigHandler).Methods("POST")
+	router.HandleFunc("/{ver}/config/", server.createConfigHandler).Methods("POST")
+	router.HandleFunc("/{ver}/group/", server.createGroupHandler).Methods("POST")
+	router.HandleFunc("/{ver}/configs/", server.getAllConfigsHandler).Methods("GET")
+	router.HandleFunc("/{ver}/groups/", server.getAllGroupsHandler).Methods("GET")
+	router.HandleFunc("/{ver}/group/{id}", server.getGroupHandler).Methods("GET")
+	router.HandleFunc("/{ver}/config/{id}", server.getConfigHandler).Methods("GET")
+	router.HandleFunc("/{ver}/config/{id}/", server.delConfigHandler).Methods("DELETE")
+	router.HandleFunc("/{ver}/group/{id}/", server.delGroupHandler).Methods("DELETE")
+	router.HandleFunc("/{ver}/group/{id}/configs", server.putConfigHandler).Methods("POST")
 
 	// start server
 	srv := &http.Server{Addr: "0.0.0.0:8000", Handler: router}
